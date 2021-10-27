c33892a0ad8683915328839f56946b109ed84f7d
munozruizjuancarlos <92393695+munozruizjuancarlos@users.noreply.github.com>
2021-10-27 16:18:59 -0500

    Add files via upload

--- sprint3/main.py
+++ sprint3/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template,url_for,redirect
+import sqlite3 
 
 app = Flask(__name__)
 
@@ -44,6 +45,26 @@ def dashboard():
 
 @app.route('/admin/buscarEmpleado',methods=['GET','POST'])
 def buscarEmpleado():
+
+    print("entre a buscar")
+    if request.method == "POST": 
+        try: 
+            w_numeroId=request.form["numeroId"]
+            w_tipo=request.form["tipo"]
+            with sqlite3.connect("db/db_mayordomo.db") as console:  
+                console.row_factory = sqlite3.Row  
+                cursor=console.cursor()  
+                statement="SELECT * FROM empleados WHERE (numeroId=?)"
+                cursor.execute(statement,(w_numeroId))  
+                rows=cursor.fetchall()
+                msg="Empleado existente en la bd"
+                return render_template("mostrarEmpleado.html",rows=rows)
+        except:
+            msg = "Registro no encontrado en la BD"
+        finally:  
+            console.close() 
+        return render_template("mostrarEmpleado.html",rows=rows) 
+
     title = "Buscar Empleado"
     return render_template('buscarEmpleado.html', title = title, nombrePag="Buscar Empleado", nombreIcono="fas fa-search")
 
@@ -56,16 +77,100 @@ def gestionarRetro():
 @app.route('/admin/crearEmpleado',methods=['GET','POST'])
 def crearEmpleado():
     title = "Crear Empleado"
+
+    msg = ""  
+    if request.method == "POST":  
+        try: 
+            print("entre")
+            w_numeroId=request.form["numeroId"]
+            w_tipo=request.form["tipo"]
+            w_nombre=request.form["nombre"]  
+            w_apellido=request.form["apellido"]  
+            w_direccion=request.form["direccion"]
+            w_telefono=request.form["telefono"]
+            w_fechaNacimiento=request.form["fechaNacimiento"]
+            w_tipoContrato=request.form["tipoContrato"]
+            w_fechaIngreso=request.form["fechaIngreso"]
+            w_cargo=request.form["cargo"]
+            w_salario=request.form["salario"]
+            w_fechaTerminoContrato=request.form["fechaTerminoContrato"]
+            w_dependencia=request.form["dependencia"]  
+            with sqlite3.connect("db/db_mayordomo.db") as console:  
+                cursor=console.cursor()  
+                statement="INSERT into empleados (numeroId,tipo,nombre,apellido,direccion,telefono,fechaNacimiento, tipoContrato,fechaIngreso,cargo,salario,fechaTerminoContrato,dependencia,rol,clave) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+                cursor.execute(statement,(w_numeroId,w_tipo,w_nombre,w_apellido,w_direccion,w_telefono,w_fechaNacimiento,w_tipoContrato,w_fechaIngreso,w_cargo,w_salario,w_fechaTerminoContrato,w_dependencia," ","plm"))                
+                console.commit()  
+                msg = "Empleado creado satisfactoriamente"  
+        except:  
+            console.rollback()  
+            msg = "No se pudo agregar el empleado a la BD"  
+        finally:  
+#            return render_template("success.html",msg = msg)  
+            msg = "Proceso finalizado"  
+
+        console.close() 
+
     return render_template('crearEmpleado.html', title = title, nombrePag="Crear Empleado", nombreIcono="fas fa-user-plus")
 
 @app.route('/admin/editarEmpleado',methods=['GET','POST'])
 def editarEmpleado():
     title = "Editar Empleado"
+
+    msg = ""  
+    if request.method == "POST":  
+        try: 
+            w_numeroId=int(request.form["numeroId"])
+            w_tipo=request.form["tipo"]
+            w_nombre=request.form["nombre"]  
+            w_apellido=request.form["apellido"]  
+            w_direccion=request.form["direccion"]
+            w_telefono=request.form["telefono"]
+            w_fechaNacimiento=request.form["fechaNacimiento"]
+            w_tipoContrato=request.form["tipoContrato"]
+            w_fechaIngreso=request.form["fechaIngreso"]
+            w_cargo=request.form["cargo"]
+            w_salario=request.form["salario"]
+            w_fechaTerminoContrato=request.form["fechaTerminoContrato"]
+            w_dependencia=request.form["dependencia"]    
+            with sqlite3.connect("db/db_mayordomo.db") as console:  
+                cursor=console.cursor()  
+                statement="UPDATE empleados set tipo=?,nombre=?,apellido=?,direccion=?,telefono=?,fechaNacimiento=?,tipoContrato=?,fechaIngreso=?,cargo=?,salario=?,fechaTerminoContrato=?,dependencia=? WHERE numeroId=?"
+                cursor.execute(statement,(w_tipo,w_nombre,w_apellido,w_direccion,w_telefono,w_fechaNacimiento,w_tipoContrato,w_fechaIngreso,w_cargo,w_salario,w_fechaTerminoContrato,w_dependencia,w_numeroId))  
+                console.commit()  
+                msg = "Empleado actualizado satisfactoriamente"  
+        except:  
+            console.rollback()  
+            msg = "No se pudo actualizar la información del empleado en la BD"  
+        finally:  
+#            return render_template("success.html",msg = msg)  
+            msg = "Proceso finalizado"  
+
+        console.close() 
+
     return render_template('editarEmpleado.html', title = title, nombrePag="Editar Empleado", nombreIcono="fas fa-user-edit")
+
 
 @app.route('/admin/eliminarEmpleado',methods=['GET','POST'])
 def eliminarEmpleado():
     title = "Eliminar Empleado"
+
+    msg=""
+    if request.method == "POST":      
+        try:  
+            w_numeroId = request.form["numeroId"]  
+            with sqlite3.connect("db/db_mayordomo.db") as console:  
+                cursor = console.cursor()  
+                cursor.execute("delete from empleados where numeroId=?",(w_numeroId,))  
+                msg = "Registro de empleado borrado satisfactoriamente"  
+        except:  
+            print(sqlite3.Error.mensaje)
+            msg = "Error en el borrado del registro"   
+        finally:  
+#           return render_template("delete_record.html",msg = msg)  
+            msg = "proceso finalizado"   
+
+        console.close()
+
     return render_template('eliminarEmpleado.html', title = title, nombrePag="Eliminar Empleado", nombreIcono="fas fa-user-slash")
 
 
